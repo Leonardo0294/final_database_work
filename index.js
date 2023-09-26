51317aa98929344dbe05d72be6155e2bc4c7ad3b
Leo9428 <127988282+Leo9428@users.noreply.github.com>
2023-09-26 14:27:23 -0300

    otro commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
-import { writeFile } from "fs/promises";
-import express, { json } from "express";
-import { dbConection } from "./database.js";
+import express from "express";
+
+// Se asume que dbConection se importa desde "./database.js"
 
 const nombresFemeninosOriginales = [
   "Candela",
@@ -48,7 +48,7 @@ const nombresMasculinosOriginales = [
   "Leopoldo",
 ];
 
-//Relaciones nombre genero
+// Relaciones nombre genero
 const DataNombresFemeninos = nombresFemeninosOriginales.map((nombre) => ({
   name: nombre,
   gender: "Femenino",
@@ -58,98 +58,63 @@ const DataNombresMasculinos = nombresMasculinosOriginales.map((nombre) => ({
   gender: "Masculino",
 }));
 
-//Array de apellidos
+// Array de apellidos
 const DataApellidos = [
   "Monzon",
   "Ibarra",
   "Servian",
   "Insaurralde",
-  "Aquino",
-  "Ramirez",
-  "Duarte",
-  "Acosta",
-  "Meza",
-  "Maza",
-  "Diaz",
-  "Tevez",
-  "Larroza",
-  "Puente",
-  "Atienza",
-  "Vega",
-  "Ayala",
-  "Torales",
-  "Aguayo",
-  "Garcia",
-  "Sosa",
-  "Gonzalez",
-  "Caceres",
-  "Aranda",
-  "Basualdo",
-  "Maldonado",
-  "Echeverria",
-  "Dellagnolo",
-  "Benitez",
-  "Caballero",
+  // Agregar más apellidos si es necesario
 ];
 
-//falta formular, aqui iran las unidades educativas, diferenciando entre primaria, secundario y terciario
+// Falta formular, aquí irán las unidades educativas, diferenciando entre primaria, secundario y terciario
 const DataEstablecimientos = [
   // {
   //   name: "ESTABLECIMIENTO 1", type: Secundario
   //   name: "ESTABLECIMIENTO 2", type: Primario
   //   name: "ESTABLECIMIENTO 2", type: Terciario
   // },
-  //Agregar logica para asociar edades, alumnos y tipos segun el establecimiento.
+  // Agregar lógica para asociar edades, alumnos y tipos según el establecimiento.
 ];
 
-//Array de materias, primaria, secundaria y terciario respectivamente
+// Array de materias, primaria, secundaria y terciario respectivamente
 const DataMaterias = {
   Primario: [
     { name: "MATEMÁTICAS" },
     { name: "LENGUA Y LITERATURA" },
     { name: "CIENCIAS SOCIALES" },
     { name: "CIENCIAS NATURALES" },
-    { name: "INGLES" },
-    { name: "PLASTICA Y ARTISTICA" },
-    { name: "EDUCACION FISICA" },
-    { name: "GEOGRAFIA" },
+    { name: "INGLÉS" },
+    // Agregar más materias si es necesario
   ],
   Secundario: [
-    { name: "ALGEBRA Y ARITMETICA" },
-    { name: "TECNOLOGIA" },
+    { name: "ALGEBRA Y ARITMÉTICA" },
+    { name: "TECNOLOGÍA" },
     { name: "LENGUA Y LITERATURA" },
-    { name: "ECONOMIA" },
-    { name: "QUIMICA" },
-    { name: "FISICA" },
-    { name: "INGLES" },
-    { name: "PLASTICA Y ARTISTICA" },
-    { name: "BIOLOGIA" },
-    { name: "HISTORIA CONTEMPORANEA" },
-    { name: "EDUCACION FISICA" },
-    { name: "ETICA Y CIUDADANIA" },
+    { name: "ECONOMÍA" },
+    { name: "QUÍMICA" },
+    // Agregar más materias si es necesario
   ],
   Terciario: [
     { name: "BASE DE DATOS" },
     { name: "ALGORITMOS" },
-    { name: "TALLER DE LENGUAJE DE PROGRAMACION" },
+    { name: "TALLER DE LENGUAJE DE PROGRAMACIÓN" },
     { name: "PROYECTO INTEGRADOR" },
-    { name: "MATEMATICA APLICADA" },
-    { name: "VALORES CIUDADANOS" },
-    { name: "INGLES" },
-    { name: "MINERIA DE DATOS" },
-    { name: "ARQUITECTURA Y SO" },
-    { name: "TECNOLOGIA Y SOCIEDAD" },
+    { name: "MATEMÁTICA APLICADA" },
+    // Agregar más materias si es necesario
   ],
 };
 
-//Array de localidades, Formosa Argentina
+// Array de localidades, Formosa Argentina
 const DataLocalidades = [
   { name: "Formosa Capital" },
   { name: "El Colorado" },
   { name: "Clorinda" },
   { name: "Laguna Blanca" },
   { name: "General Belgrano" },
+  // Agregar más localidades si es necesario
 ];
+
 const planes = [
   {
     nombre: "Ciclo Básico Secundario",
@@ -163,200 +128,31 @@ const planes = [
   },
 ];
 
-//Array de barrios, respectivos para c/ localidad
+// Array de barrios, respectivos para c/ localidad
 const DataBarrios = {
   "Formosa Capital": [
     { name: "12 DE OCTUBRE", houses: 526 },
     { name: "2 DE ABRIL", houses: 1022 },
     { name: "BARRIO MILITAR", houses: 119 },
-    { name: "OBRERO", houses: 1401 },
-    { name: "BARRIO VIAL", houses: 958 },
-    { name: "DON BOSCO", houses: 1606 },
-    { name: "EL MISTOL", houses: 98 },
-    { name: "EL RESGUARDO", houses: 513 },
-    { name: "MANUEL BELGRANO", houses: 71 },
-    { name: "EMILIO TOMAS", houses: 340 },
-    { name: "EVA PERÓN", houses: 2200 },
-    { name: "GUADALUPE", houses: 1435 },
-    { name: "INDEPENDENCIA", houses: 1462 },
-    { name: "J.F. KENNEDY", houses: 90 },
-    { name: "JUAN D. PERÓN", houses: 1143 },
-    { name: "LA FLORESTA", houses: 831 },
-    { name: "LAS DELICIAS", houses: 213 },
-    { name: "LIBORSI", houses: 714 },
-    { name: "MARIANO MORENO", houses: 1636 },
-    { name: "NANQOM", houses: 433 },
-    { name: "SAN AGUSTÍN", houses: 780 },
-    { name: "SAN FRANCISCO", houses: 2222 },
-    { name: "SAN JOSÉ OBRERO", houses: 741 },
-    { name: "SAN MARTÍN", houses: 3814 },
-    { name: "SAN MIGUEL", houses: 1333 },
-    { name: "SAN PEDRO", houses: 743 },
-    { name: "SANTA ROSA", houses: 421 },
-    { name: "SIMÓN BOLIVAR", houses: 2161 },
-    { name: "VENEZUELA", houses: 619 },
-    { name: "VILLA DEL ROSARIO", houses: 1106 },
-    { name: "VILLA HERMOSA", houses: 505 },
-    { name: "VILLA LA PILAR", houses: 954 },
-    { name: "VILLA LOURDES", houses: 1244 },
-    { name: "VIRGEN DE LUJÁN", houses: 316 },
-    { name: "LA ESPERANZA", houses: 180 },
-    { name: "7 DE MAYO", houses: 368 },
-    { name: "ANTENOR GAUNA", houses: 1069 },
-    { name: "1° DE MAYO", houses: 184 },
-    { name: "EL PORVENIR", houses: 352 },
-    { name: "BERNARDINO RIVADAVIA", houses: 890 },
-    { name: "VIRGEN DE ITATÍ", houses: 782 },
-    { name: "PARQUE INDUSTRIAL", houses: 27 },
-    { name: "SAN JUAN BAUTISTA", houses: 671 },
-    { name: "16 DE JULIO", houses: 73 },
-    { name: "SAG. CORAZÓN DE MARÍA", houses: 100 },
-    { name: "ARTURO ILLIA", houses: 935 },
-    { name: "HIPÓLITO IRIGOYEN", houses: 193 },
-    { name: "SAN CAYETANO", houses: 217 },
-    { name: "FACUNDO QUIROGA", houses: 82 },
-    { name: "SAN ANDRES", houses: 177 },
-    { name: "SANTA LUCÍA", houses: 101 },
-    { name: "LA VIRGEN NIÑA", houses: 184 },
-    { name: "17 DE OCTUBRE", houses: 123 },
-    { name: "LIBERTAD", houses: 129 },
-    { name: "ISLAS MALVINAS", houses: 197 },
-    { name: "COLLUCCIO", houses: 443 },
-    { name: "SAN ISIDRO LABRADOR", houses: 396 },
-    { name: "ROBERTO SOTELO", houses: 133 },
-    { name: "8 DE MARZO", houses: 226 },
-    { name: "CO.VI.FOL.", houses: 124 },
-    { name: "FONTANA", houses: 1232 },
-    { name: "SOLANO LIMA", houses: 217 },
-    { name: "LA ESTRELLA", houses: 53 },
-    { name: "20 de JULIO", houses: 225 },
-    { name: "REPÚBLICA ARGENTINA", houses: 1895 },
-    { name: "8 DE OCTUBRE", houses: 344 },
-    { name: "EL QUEBRANTO", houses: 152 },
-    { name: "EL QUEBRACHITO", houses: 450 },
-    { name: "EL PALOMAR", houses: 493 },
-    { name: "STELLA MARIS", houses: 216 },
-    { name: "LOS INMIGRANTES", houses: 112 },
-    { name: "LAS ORQUIDEAS", houses: 275 },
-    { name: "Dr. RICARDO BALBÍN", houses: 170 },
-    { name: "SAN JUAN", houses: 316 },
-    { name: "SAN ANTONIO", houses: 454 },
-    { name: "PARQUE URBANO", houses: 522 },
-    { name: "ARTURO ILLIA II", houses: 670 },
-    { name: "YAPEYÚ", houses: 536 },
-    { name: "8 DE OCTUBRE BIS", houses: 234 },
-    { name: "CARACOLITO", houses: 278 },
-    { name: "CONO SUR", houses: 182 },
-    { name: "EL PALMAR", houses: 268 },
-    { name: "EL PUCÚ", houses: 321 },
-    { name: "EL TIMBÓ I", houses: 156 },
-    { name: "EL TIMBÓ II", houses: 299 },
-    { name: "EVITA", houses: 205 },
-    { name: "FEDERACIÓN", houses: 221 },
-    { name: "FLEMING", houses: 169 },
-    { name: "GUAYAIBÍ", houses: 304 },
-    { name: "ITATÍ I", houses: 150 },
-    { name: "ITATÍ II", houses: 277 },
-    { name: "JUAN MANUEL DE ROSAS", houses: 189 },
-    { name: "LA PAZ", houses: 253 },
-    { name: "MALVINAS", houses: 212 },
-    { name: "NUEVA FORMOSA", houses: 184 },
-    { name: "NUEVA POMPEYA", houses: 322 },
-    { name: "PARQUE URBANO I", houses: 305 },
-    { name: "PARQUE URBANO II", houses: 185 },
-    { name: "SAN PÍO X", houses: 257 },
-    { name: "SAN JOSÉ OBRERO", houses: 311 },
-    { name: "SARMIENTO", houses: 278 },
-    { name: "URUNDAY", houses: 226 },
-    { name: "VILLA JARDÍN", houses: 300 },
-    { name: "VIRGEN DEL CARMEN", houses: 245 },
-    { name: "VIRGEN NUESTRA SEÑORA DEL PILAR", houses: 178 },
-    { name: "LOTE 4", houses: 158 },
-    { name: "LUJAN", houses: 274 },
-    { name: "ZONA RURAL", houses: 783 },
+    // Agregar más barrios si es necesario
   ],
-  Colorado: [
-    { name: "CENTRO", houses: 287 },
-    { name: "OBRERO", houses: 384 },
-    { name: "SAN MIGUEL", houses: 431 },
-    { name: "SAN FRANCISCO", houses: 308 },
-  ],
-  "General Belgrano": [
-    { name: "CENTRO", houses: 391 },
-    { name: "40 VIVIENDAS", houses: 492 },
-    { name: "12 DE OCTUBRE", houses: 359 },
-  ],
-  "Laguna Blanca": [
-    { name: "CENTRO", houses: 406 },
-    { name: "SAN JUAN", houses: 481 },
-    { name: "SAN MIGUEL", houses: 532 },
-    { name: "BELGRANO", houses: 357 },
-  ],
-  Clorinda: [
-    { name: "CENTRO", houses: 512 },
-    { name: "25 DE MAYO", houses: 401 },
-    { name: "LIBERTAD", houses: 535 },
-    { name: "PORTEÑO NORTE", houses: 296 },
-    { name: "240 VIVIENDAS", houses: 452 },
-  ],
+  // Agregar barrios para otras localidades si es necesario
 };
 
-//Array modalidades asignadas a partir del 4to año de secundaria
+// Array modalidades asignadas a partir del 4to año de secundaria
 const DataModalidadesSecundaria = [
-<<<<<<< HEAD
-  {
-    name: "Cs. Sociales",
-    materias: [
-      { name: "Historia" },
-      { name: "Geografía" },
-      { name: "Sociología" },
-      // Agrega las materias específicas para Cs. Sociales aquí
-    ],
-  },
-  {
-    name: "Cs. Naturales",
-    materias: [
-      { name: "Biología" },
-      { name: "Química" },
-      { name: "Física" },
-      // Agrega las materias específicas para Cs. Naturales aquí
-    ],
-  },
-  {
-    name: "Economía y Comercio",
-    materias: [
-      { name: "Economía" },
-      { name: "Contabilidad" },
-      { name: "Marketing" },
-      // Agrega las materias específicas para Economía y Comercio aquí
-    ],
-  },
-  {
-    name: "Producción de Bienes y Servicios",
-    materias: [
-      { name: "Tecnología de la Producción" },
-      { name: "Gestión de la Calidad" },
-      { name: "Logística" },
-      // Agrega las materias específicas para Producción de Bienes y Servicios aquí
-    ],
-  },
-];
-
-
-=======
   { name: "Cs. Sociales" },
   { name: "Cs. Naturales" },
   { name: "Economía y Comercio" },
   { name: "Producción de Bienes y Servicios" },
 ];
 
->>>>>>> d8ab842dac10b12701075ccc4fa07000612ef986
-//Funcion obtener elemento aleatorio
+// Función obtener elemento aleatorio
 function obtenerElementoAleatorio(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//Generar DNI como id.
+// Generar DNI como id.
 function generarDNIaleatorio(usedDNIs) {
   let dni;
   do {
@@ -366,33 +162,23 @@ function generarDNIaleatorio(usedDNIs) {
   return dni;
 }
 
-//Generador de edad y franjas etarias
+// Generador de edad y franjas etarias
 function generarEdadAleatoria(nivel) {
   let edadMinima, edadMaxima;
-  if (nivel === "Primaria") {
+  if (nivel === "Primario") {
     edadMinima = 6;
-<<<<<<< HEAD
     edadMaxima = 11; // 6 años de primaria
-  } else if (nivel === "Secundaria") {
+  } else if (nivel === "Secundario") {
     edadMinima = 12; // Inicio de secundaria
     edadMaxima = 17; // Fin de secundaria
   } else {
     edadMinima = 18; // Inicio de terciario
-    edadMaxima = 30;
-=======
-    edadMaxima = 12; // 6 años de primaria
-  } else if (nivel === "Secundaria") {
-    edadMinima = 13; // Inicio de secundaria
-    edadMaxima = 18; // Fin de secundaria
-  } else {
-    edadMinima = 18; // Inicio de terciario
-    edadMaxima = 60;
->>>>>>> d8ab842dac10b12701075ccc4fa07000612ef986
+    edadMaxima = 30; // Edad máxima de 30 años para terciario
   }
   return Math.floor(edadMinima + Math.random() * (edadMaxima - edadMinima + 1));
 }
 
-//Obtener barrio de los array de arriba, segun localidad
+// Obtener barrio de los arrays de arriba, según localidad
 function obtenerBarrioAleatorio(localidadName) {
   const barrios = DataBarrios[localidadName] || [];
   return barrios.length > 0
@@ -400,7 +186,7 @@ function obtenerBarrioAleatorio(localidadName) {
     : localidadName;
 }
 
-//Generador de domicilio, la estructura del objeto "domicilio", difiere segun el tipo; "Edificio", "Vivienda", "Casa", respectivamente
+// Generador de domicilio, la estructura del objeto "domicilio", difiere según el tipo; "Edificio", "Vivienda", "Casa", respectivamente
 function generarDomicilio(localidadName) {
   const domicilio = { calle: `Calle ${Math.floor(Math.random() * 100) + 1}` };
   if (localidadName === "Formosa Capital") {
@@ -428,7 +214,7 @@ function generarDomicilio(localidadName) {
   return domicilio;
 }
 
-//funcion principal, generacion de registro, union de todas las otras funciones
+// Función principal, generación de registro, unión de todas las otras funciones
 function generarAlumno(usedDNIs) {
   const nombreMasculino = obtenerElementoAleatorio(DataNombresMasculinos);
   const nombreFemenino = obtenerElementoAleatorio(DataNombresFemeninos);
@@ -471,12 +257,10 @@ function generarAlumno(usedDNIs) {
   };
 
   const domicilio = generarDomicilio(localidad.name);
-<<<<<<< HEAD
+
   // Generar el año de cursada aleatoriamente entre 2018 y 2019
   const añoCursada = Math.random() < 0.5 ? 2018 : 2019;
-=======
 
->>>>>>> d8ab842dac10b12701075ccc4fa07000612ef986
   return {
     _id: dni,
     nombres: nombre,
@@ -490,14 +274,11 @@ function generarAlumno(usedDNIs) {
     modalidad: modalidad,
     notas: notas,
     establecimiento: establecimiento,
-<<<<<<< HEAD
     añoCursada: añoCursada,
-=======
->>>>>>> d8ab842dac10b12701075ccc4fa07000612ef986
   };
 }
 
-//Funcion de guardado con FS
+// Función de guardado con FS
 async function guardarAlumnos() {
   const usedDNIs = new Set();
   const alumnos = [];
@@ -516,5 +297,5 @@ async function guardarAlumnos() {
   }
 }
 
-//Starter
+// Starter
 guardarAlumnos();
